0eb4a8894e255b65821ac95cf902530b3b375a1f
Fabio Maia <fabioromm@gmail.com>
2018-06-17 23:28:45 -0300

    Basic rom loading and some instructions implemented

--- README.md
+++ README.md
@@ -1,1 +1,74 @@
+# References
+
+* https://github.com/badlogic/chip8/blob/master/roms/pong.rom
+* http://devernay.free.fr/hacks/chip8/C8TECH10.HTM#memmap
+* http://www.multigesture.net/articles/how-to-write-an-emulator-chip-8-interpreter/
+
 # ruby-chip-8-emulator
+
+Memory
+ - 4KB (4,096 bytes)
+ - 0x000 (0) to 0xFFF (4095)
+ - The first 512 bytes, from 0x000 to 0x1FF, reserved space
+ - most ROMs start at 0x200 (512) but some begin at 0x600 (1536). Programs beginning at 0x600 are intended for the ETI 660 computer.
+
+  0x000-0x1FF - Chip 8 interpreter (contains font set in emu)
+  0x050-0x0A0 - Used for the built in 4x5 pixel font set (0-F)
+  0x200-0xFFF - Program ROM and work RAM
+  
+---
+  
+ROM Registers
+ - 16 registers
+ - 8 bit (1Byte) each
+ - usually referred to as Vx, where x is a hexadecimal digit (0 through F)
+ - VF not used by ROMs, as it is used as a flag by some instructions.
+ 
+Store Memory Addresses Register
+ - Special 16-bit register called I. This register is generally used to store memory addresses, so only the lowest (rightmost) 12 bits are usually used
+ 
+Sound and Delay Timer Registers
+ - Two additional registers for the delay and sound timers. When these registers are non-zero, they are automatically decremented at a rate of 60Hz.
+ 
+Program Counter Register 
+ - The program counter (PC) should be 16-bit, and is used to store the currently executing address.
+ 
+Stack and Stack Register
+ - The stack pointer (SP) can be 8-bit, it is used to point to the topmost level of the stack. 
+ - The stack is an array of 16 16-bit values, used to store the address that the interpreter should return to when finished with a subroutine.
+
+---
+
+Inputs
+ - 16 Keys
+ - 0 - F hexadecimal
+ 
+---
+
+Display
+
+ - 64x32-pixel monochrome
+ (0,0)       (63,0)
+ (0,31)      (63,31)
+ 
+Sprites
+ - 15 bytes. for a possible sprite size of 8x15
+ - fonts: 5 bytes or 8x5 pixels. Stored in (0x000 to 0x1FF)
+ 
+--------
+Delay and Sound Timers
+
+Chip-8 provides 2 timers, a delay timer and a sound timer.
+
+The delay timer is active whenever the delay timer register (DT) is non-zero. This timer does nothing more than subtract 1 from the value of DT at a rate of 60Hz. When DT reaches 0, it deactivates.
+
+The sound timer is active whenever the sound timer register (ST) is non-zero. This timer also decrements at a rate of 60Hz, however, as long as ST's value is greater than zero, the Chip-8 buzzer will sound. When ST reaches zero, the sound timer deactivates.
+
+The sound produced by the Chip-8 interpreter has only one tone. The frequency of this tone is decided by the author of the interpreter.
+
+
+-----
+Instructions
+ - 2 bytes
+ 
+ 
---
+++ src/application.rb
@@ -0,0 +1,667 @@
+class OverflowError < StandardError
+
+end
+
+class Display
+  def clear
+
+  end
+end
+
+class DumbDisplay < Display
+  def initialize
+    @pixels = Array.new(2048, 0x0)
+  end
+
+  def clear
+    @pixels = Array.new(2048, 0x0)
+  end
+end
+
+class Memory
+  def initialize(size_in_bytes:, item_size_in_bits:)
+    @buffer = Array.new(size_in_bytes, 0x0)
+    @item_size_in_bits = item_size_in_bits
+  end
+
+  def write(position, data)
+    validate_position!(position)
+    validate_data!(data)
+    @buffer[position] = data
+  end
+
+  # @param [Register] register
+  # @param [Integer] length
+  def read(register:, length: 1)
+    validate_position!(register.read)
+    validate_position!(register.read + length)
+    @buffer[(register.read)..(register.read+length)].inject(0x0000) {|acc, byte| (acc << 8) | byte} >> 8
+  end
+
+  def to_s
+    "#{@buffer}"
+  end
+
+  def validate_position!(position)
+    raise OverflowError unless position >= 0 && position < @buffer.size
+  end
+
+  private :validate_position!
+
+  def validate_data!(data)
+    raise OverflowError unless (data >> @item_size_in_bits) == 0
+  end
+
+  private :validate_data!
+end
+
+class Ram < Memory
+  def initialize
+    super(size_in_bytes: 4_096, item_size_in_bits: 8)
+  end
+end
+
+class Vram < Memory
+  def initialize
+    super(size_in_bytes: 2_048, item_size_in_bits: 1)
+  end
+end
+
+
+class Stack
+  def initialize(levels:, item_size_in_bits:)
+    @addresses = []
+    @levels = levels
+    @item_size_in_bits = item_size_in_bits
+  end
+
+  def push(data)
+    raise OverflowError if (data >> @item_size_in_bits) > 0 || @addresses.size >= @levels
+    @addresses.push(data)
+  end
+
+  def to_s
+    "STACK: #{@addresses}"
+  end
+end
+
+class Register
+  def initialize(size_in_bits:, initial_data: 0x0, name:)
+    raise OverflowError if (initial_data >> size_in_bits) > 0
+    @data = initial_data
+    @size_in_bits = size_in_bits
+    @name = name
+  end
+
+  def update(data)
+    raise OverflowError if (data >> @size_in_bits) > 0
+    @data = data
+  end
+
+  def read
+    @data
+  end
+
+  def add(data)
+    @data += data
+  end
+
+  def ==(data)
+    @data == data
+  end
+
+  def to_s
+    "#{@name}: #{@data}"
+  end
+end
+
+class Registers
+  def initialize(count:, register_size_in_bits:, name: "REGISTERS")
+    @registers = Array.new(count) { |index| Register.new(size_in_bits: register_size_in_bits, name: "V#{index}") }
+    @name = name
+  end
+
+  def read_register(register_index)
+    raise OverflowError unless register_index >= 0 && register_index < @registers.size
+    @registers[register_index]
+  end
+
+  def update_register(register_index, data)
+    raise OverflowError unless register_index >= 0 && register_index < @registers.size
+    @registers[register_index].update(data)
+  end
+
+  def to_s
+    "#{@name}:\n#{@registers.map(&:to_s).join("\n")}"
+  end
+end
+
+
+class ProgramCounter < Register
+  def initialize
+    super(size_in_bits: 16, initial_data: 0x200, name: "PC")
+  end
+end
+
+class MemoryAddress < Register
+  def initialize
+    super(size_in_bits: 12, name: "I")
+  end
+end
+
+class Instruction
+  def execute(opcode)
+
+  end
+end
+
+class ClearDisplay < Instruction
+  # @param [Display] display
+  def initialize(display:)
+    @display = display
+  end
+
+  def execute(opcode)
+    return unless opcode == 0x00E0
+    @display.clear
+  end
+end
+
+class ReturnFromSubroutine < Instruction
+  # @param [Stack] stack
+  # @param [ProgramCounter] pc
+  def initialize(stack:, pc:)
+    @stack = stack
+    @pc = pc
+  end
+
+  def execute(opcode)
+    return unless opcode == 0x00EE
+    puts "TODO: implement me"
+  end
+end
+
+class Jump < Instruction
+  # @param [ProgramCounter] pc
+  def initialize(pc:)
+    @pc = pc
+  end
+
+  def execute(opcode)
+    return unless opcode & 0xF000 == 0x1000
+    @pc.update(opcode & 0x0FFF)
+  end
+end
+
+class Call < Instruction
+  # @param [ProgramCounter] pc
+  # @param [Stack] stack
+  def initialize(pc:, stack:)
+    @pc = pc
+    @stack = stack
+  end
+
+  def execute(opcode)
+    return unless opcode & 0xF000 == 0x2000
+    @stack.push(@pc.read)
+    @pc.update(opcode & 0x0FFF)
+  end
+end
+
+class SkipRegisterEqualsValue < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 3xkk => Vx == kk
+    return unless opcode & 0xF000 == 0x3000
+    register_index = (opcode & 0x0F00) >> 8
+    @pc.add(2) if @registers.read_register(register_index) == opcode & 0x00FF
+
+  end
+end
+
+class SkipRegisterNotEqualsValue < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 4xkk => Vx != kk
+    return unless opcode & 0xF000 == 0x4000
+    register_index = (opcode & 0x0F00) >> 8
+    @pc.add(2) if @registers.read_register(register_index) != opcode & 0x00FF
+  end
+end
+
+class SkipRegisterEqualsRegister < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 5xy0 => Vx == kk
+    return unless opcode & 0xF000 == 0x5000
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+    @pc.add(2) if @registers.read_register(first_register_index) == @registers.read_register(second_register_index)
+  end
+end
+
+class UpdateRegister < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 6xkk => Vx <= kk
+    return unless opcode & 0xF000 == 0x6000
+    register_index = (opcode & 0x0F00) >> 8
+    value = opcode & 0x00FF
+    @registers.update_register(register_index, value)
+    @pc.add(2)
+  end
+end
+
+class AddToRegister < Instruction
+  # @param [Registers] registers
+  # # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 7xkk => Vx = Vx + kk
+    return unless opcode & 0xF000 == 0x7000
+    register_index = (opcode & 0x0F00) >> 8
+    value = opcode & 0x00FF
+    current_value = @registers.read_register(register_index).read
+    @registers.update_register(register_index, value + current_value)
+    @pc.add(2)
+  end
+end
+
+class Copy < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy0 => Vx = Vy
+    return unless opcode & 0xF00F == 0x8000
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(first_register_index, second_register_value)
+    @pc.add(2)
+  end
+end
+
+class Or < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy1 => Vx = Vx | Vy
+    return unless opcode & 0xF00F == 0x8001
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(first_register_index, first_register_value | second_register_value)
+    @pc.add(2)
+  end
+end
+
+
+class And < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy2 => Vx = Vx & Vy
+    return unless opcode & 0xF00F == 0x8002
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(first_register_index, first_register_value & second_register_value)
+    @pc.add(2)
+  end
+end
+
+class Xor < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy3 => Vx = Vx ^ Vy
+    return unless opcode & 0xF00F == 0x8003
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(first_register_index, first_register_value ^ second_register_value)
+    @pc.add(2)
+  end
+end
+
+class SumRegisters < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy4 => Vx = Vx + Vy, VF = carry
+    return unless opcode & 0xF00F == 0x8004
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    sum = first_register_value + second_register_value
+    @registers.update_register(first_register_index, sum & 0xFF)
+    @registers.update_register(0x0F, 0x01) if sum >> 8 > 0
+    @pc.add(2)
+  end
+end
+
+class SubRegisters < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy5 => Vx = Vx - Vy, VF = NOT borrow
+    return unless opcode & 0xF00F == 0x8005
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(0x0F, 0x01) if first_register_value > second_register_value
+    @registers.update_register(first_register_index, first_register_value - second_register_value)
+    @pc.add(2)
+  end
+end
+
+class ShiftRightRegisters < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy6 => Vx=Vy>>1,  VF = Vy & 0x01
+    return unless opcode & 0xF00F == 0x8006
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(0x0F, second_register_value & 0x01)
+    @registers.update_register(first_register_index, second_register_value >> 0x01)
+    @pc.add(2)
+  end
+end
+
+class SubbRegisters < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xy5 => Vx = Vy - Vx, VF = NOT borrow
+    return unless opcode & 0xF00F == 0x8007
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    first_register_value = @registers.read_register(first_register_index).read
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(0x0F, 0x01) if second_register_value > first_register_value
+    @registers.update_register(first_register_index, second_register_value - first_register_value)
+    @pc.add(2)
+  end
+end
+
+class ShiftLeftRegisters < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 8xyE => Vx=Vy<<1,  VF = Vy & 0x01
+    return unless opcode & 0xF00F == 0x800E
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+
+    second_register_value = @registers.read_register(second_register_index).read
+    @registers.update_register(0x0F, second_register_value & 0x80 != 0 ? 0x01 : 0x00)
+    @registers.update_register(first_register_index, second_register_value << 0x01)
+    @pc.add(2)
+  end
+end
+
+class SkipRegisterNotEqualsRegister < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # 9xy0 => Vx != Vy
+    return unless opcode & 0xF00F == 0x9000
+    first_register_index = (opcode & 0x0F00) >> 8
+    second_register_index = (opcode & 0x00F0) >> 4
+    @pc.add(2) if @registers.read_register(first_register_index) != @registers.read_register(second_register_index)
+  end
+end
+
+class SetMemoryAddressRegister < Instruction
+  # @param [MemoryAddress] ma
+  # @param [ProgramCounter] pc
+  def initialize(ma:, pc:)
+    @ma = ma
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # Annn
+    return unless opcode & 0xF000 == 0xA000
+    address = opcode & 0x0FFF
+    @ma.update(address)
+    @pc.add(2)
+  end
+end
+
+class JumpToV0 < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # Bnnn
+    return unless opcode & 0xF000 == 0xB000
+    @pc.add((opcode & 0x0FFF) + @registers.read_register(0x0).read)
+  end
+end
+
+class RandToRegister < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  def initialize(registers:, pc:)
+    @registers = registers
+    @pc = pc
+  end
+
+  def execute(opcode)
+    # Cxkk
+    return unless opcode & 0xF000 == 0xC000
+    register_index = (opcode & 0x0F00) >> 8
+    value = (opcode & 0x00FF) & rand(0xFF)
+    @registers.update_register(register_index, value)
+    @pc.add(2)
+  end
+end
+
+class Draw < Instruction
+  # @param [Registers] registers
+  # @param [ProgramCounter] pc
+  # @param [Vram] vram
+  def initialize(registers:, pc:, vram:)
+    @registers = registers
+    @pc = pc
+    @vram = vram
+  end
+
+  def execute(opcode)
+    # Dxyn
+    return unless opcode & 0xF000 == 0xD000
+    # register_index = (opcode & 0x0F00) >> 8
+    # value = (opcode & 0x00FF) & rand(0xFF)
+    # @registers.update_register(register_index, value)
+    # TODO: implement me
+    @pc.add(2)
+  end
+end
+
+display = DumbDisplay.new
+pc = ProgramCounter.new
+
+ma = MemoryAddress.new
+stack = Stack.new(levels: 16, item_size_in_bits: 16)
+registers = Registers.new(count: 16, register_size_in_bits: 8)
+ram = Ram.new
+vram = Vram.new
+
+debug = false
+if debug
+  ClearDisplay.new(display: display).execute(0x0)
+  ReturnFromSubroutine.new(stack: stack, pc: pc).execute(0x0)
+  Jump.new(pc: pc).execute(0xFFFF)
+  Call.new(stack: stack, pc: pc).execute(0xFFFF)
+  SkipRegisterEqualsValue.new(registers: registers, pc: pc).execute(0xFFFF)
+  SkipRegisterNotEqualsValue.new(registers: registers, pc: pc).execute(0xFFFF)
+  SkipRegisterEqualsRegister.new(registers: registers, pc: pc).execute(0xFFFF)
+  UpdateRegister.new(registers: registers, pc: pc).execute(0xFFFF)
+  AddToRegister.new(registers: registers, pc: pc).execute(0xF104)
+  Copy.new(registers: registers, pc: pc).execute(0xF210)
+  Or.new(registers: registers, pc: pc).execute(0x8411)
+  And.new(registers: registers, pc: pc).execute(0x8512)
+  Xor.new(registers: registers, pc: pc).execute(0x8213)
+  SumRegisters.new(registers: registers, pc: pc).execute(0x8144)
+  SubRegisters.new(registers: registers, pc: pc).execute(0x8145)
+  ShiftRightRegisters.new(registers: registers, pc: pc).execute(0x8246)
+  SubbRegisters.new(registers: registers, pc: pc).execute(0x8246)
+  ShiftLeftRegisters.new(registers: registers, pc: pc).execute(0x8426)
+  SkipRegisterNotEqualsRegister.new(registers: registers, pc: pc).execute(0xFFFF)
+  SetMemoryAddressRegister.new(ma: ma, pc: pc).execute(0xA333)
+  JumpToV0.new(registers: registers, pc: pc).execute(0xB222)
+  RandToRegister.new(registers: registers, pc: pc).execute(0xC7FF)
+  Draw.new(registers: registers, pc: pc, vram: vram).execute(0xD003)
+
+  puts pc
+  puts ma
+  puts stack
+  puts registers
+else
+
+  contents = IO.binread(File.join(__dir__, '../roms/pong.rom'))
+
+  write_position = pc.read
+  contents.each_byte do |b|
+    ram.write(write_position, b)
+    write_position+=1
+  end
+
+  instructions = [
+      ClearDisplay.new(display: display),
+      ReturnFromSubroutine.new(stack: stack, pc: pc),
+      Jump.new(pc: pc),
+      Call.new(stack: stack, pc: pc),
+      SkipRegisterEqualsValue.new(registers: registers, pc: pc),
+      SkipRegisterNotEqualsValue.new(registers: registers, pc: pc),
+      SkipRegisterEqualsRegister.new(registers: registers, pc: pc),
+      UpdateRegister.new(registers: registers, pc: pc),
+      AddToRegister.new(registers: registers, pc: pc),
+      Copy.new(registers: registers, pc: pc),
+      Or.new(registers: registers, pc: pc),
+      And.new(registers: registers, pc: pc),
+      Xor.new(registers: registers, pc: pc),
+      SumRegisters.new(registers: registers, pc: pc),
+      SubRegisters.new(registers: registers, pc: pc),
+      ShiftRightRegisters.new(registers: registers, pc: pc),
+      SubbRegisters.new(registers: registers, pc: pc),
+      ShiftLeftRegisters.new(registers: registers, pc: pc),
+      SkipRegisterNotEqualsRegister.new(registers: registers, pc: pc),
+      SetMemoryAddressRegister.new(ma: ma, pc: pc),
+      JumpToV0.new(registers: registers, pc: pc),
+      RandToRegister.new(registers: registers, pc: pc),
+      Draw.new(registers: registers, pc: pc, vram: vram)
+  ]
+  while true
+    code = ram.read(register: pc, length: 2)
+    puts "OPCODE: #{code.to_s(16)}"
+    instructions.each {|instruction| instruction.execute(code)}
+
+    puts pc
+    puts ma
+    puts stack
+    puts registers
+    puts "waiting for input"
+    next gets == "n"
+    break if gets == "q"
+  end
+end
+
